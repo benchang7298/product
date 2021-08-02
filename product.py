a014afdf210faef3b89cd771589313b7ba227c4a
Ben Chang <ben.chang@intel.com>
2021-08-02 14:41:38 +0800

    Add files via upload

--- product.py
+++ product.py
@@ -1,4 +1,8 @@
 product = []
+#with open('product.csv', 'r') as f:
+#    for line in f:
+#        s = line.strip().split(',')
+#        print(s)
 while True:
     name = input('please enter the name of product: ')
     if name == 'q':
@@ -9,3 +13,8 @@ print(product)
 
 for p in product:
     print(p[0], 'price is', p[1])
+
+with open('product.csv', 'w', encoding='utf-8') as f:
+    f.write('PRODUCT,PRICE\n')
+    for p in product:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
